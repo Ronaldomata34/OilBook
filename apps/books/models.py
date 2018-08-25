792bd89bab271dafacffc2313e317d00b1cc71bd
Ronaldo <ronaldomata34@gmail.com>
2018-08-25 18:37:05 -0400

    project bases

---
+++ apps/books/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Book(models.Model):
+	title = models.CharField(max_length=55, unique=True)
+	author = models.CharField(max_length=55, blank=True)
+	url = models.URLField(unique=True)
+	image = models.ImageField(upload_to="img/books")
+	subject = models.CharField(max_length=55)
+
+	class Meta:
+		ordering = ['title']
+
+	def __str__(self):
+		return self.title
+
+	def this_subject(self, subject):
+		if self.subject == subject.
+			return True
+		return False
+
+
